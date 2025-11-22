eed17d13c2c295ce8c47490298308f39ae089747
oscarngncc <innonion@gmail.com>
2025-11-22 20:07:58 +0000

    disable security header injection for now

--- run-all.sh
+++ run-all.sh
@@ -6,7 +6,6 @@ cp -r ../Obsidiandocs/* docs/
 python3 ./gen_index_md.py
 python3 ./gen_graph.py
 python3 -m mkdocs build --no-directory-urls
-python3 ./inject_security_headers.py
 
 wslview site/index.html
 
